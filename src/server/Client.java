4907ac192a53a3a4cb548f98e0acd74d128e29a1
kevin <kevin@BOARDROOM>
2019-04-26 03:16:24 +0200

    Påbörjat olika klasser, inget är klart

---
+++ src/server/Client.java
@@ -0,0 +1,13 @@
+package server;
+
+import java.net.Socket;
+
+public class Client {
+	private Socket socket;
+	
+	public Client(Socket socket) {
+		this.socket = socket;
+	}
+	
+	
+}
--- src/server/Connection.java
+++ src/server/Connection.java
@@ -1,5 +1,31 @@
 package server;
 
-public class Connection {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class Connection {
+	private int port;
+	private Handler handler ;
+	
+	public Connection(int port) {
+		this.port = port;
+		
+	}
+	
+	private class Handler extends Thread{
+		public Handler() {
+			
+		}
+		
+		public void run() {
+			try {
+				ServerSocket sSocket = new ServerSocket();
+				while(true) {
+					
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
