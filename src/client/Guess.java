650cfdcdb4eb58954ca0075b1a524c6a825f9d4b
Kevin Stjernborg <kevin@BOARDROOM>
2019-04-26 02:38:28 +0200

    Kommenterat litta, gjort en klass som tänkt ska skickas till servern

--- src/client/Guess.java
+++ src/client/Guess.java
@@ -1,16 +1,21 @@
 package client;
 
-public class Coordinate {
+public class Guess {
 	private double x;
 	private double y;
+	private double kilometers;
+
 	
-	
-	public Coordinate(double x, double y) {
+	public Guess(double x, double y) {
 		this.x = x;
 		this.y = y;	
 	}
 	
 	public String toString() {
 		return "Coordinate [x=" + x + ", y=" + y + "]";
+	}
+	
+	public void setDistance(Double kilometers) {
+		this.kilometers = kilometers;
 	}
 }
