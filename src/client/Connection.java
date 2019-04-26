37c0916268361adf08b9b218dfc8f17b784117f4
kevin <kevin@BOARDROOM>
2019-04-26 02:43:20 +0200

    Påbörjat klass för uppkoppling

--- src/client/Connection.java
+++ src/client/Connection.java
@@ -1,5 +1,39 @@
 package client;
 
-public class Connection {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+
+
+public class Connection {
+	private Socket socket;
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
+	private int port;
+	private String address;
+	
+	
+	public Connection(String address, int port) {
+		this.address = address;
+		this.port = port;
+	}
+	
+	public void sendGuess(Guess guess) {
+		try {
+			socket = new Socket(address, port);
+			ois = new ObjectInputStream(socket.getInputStream());
+			oos = new ObjectOutputStream(socket.getOutputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public void connect() {
+		
+	}
 }
