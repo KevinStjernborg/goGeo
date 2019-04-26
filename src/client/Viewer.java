9569e397f8b832fd8e52baf4365b9a70e690ed5f
kevin <kevin@BOARDROOM>
2019-04-26 02:38:38 +0200

    Kommenterat

--- src/client/Viewer.java
+++ src/client/Viewer.java
@@ -34,18 +34,18 @@ import org.jxmapviewer.viewer.TileFactoryInfo;
 import org.jxmapviewer.viewer.Waypoint;
 import org.jxmapviewer.viewer.WaypointPainter;
 
-
-
 public class Viewer {
 	private static int eventCnt;
 	private JXMapViewer viewer;
 	private Timer timer;
 	private GeoPosition Paris = new GeoPosition(48.8566, 2.3522);
 
-
 	
 	
-
+	
+	/**
+	 * Constructor for the class
+	 */
 	public Viewer() {
 		/*
 		 * initierar kartdelen
@@ -62,38 +62,37 @@ public class Viewer {
 		viewer.setZoom(16);
 		addDoubleClick();
 		timer = new Timer();
-		
-		
-		/*
-		 * för knappar
-		 */
-		
 
-		
-		
+
 	}
-
+	/**
+	 * Adds a marker on the map that shows where the player clicked
+	 * @param p1
+	 */
 	public void addLocation(GeoPosition p1) {
 		Set<SwingWaypoint> waypoints = new HashSet<SwingWaypoint>(Arrays.asList(new SwingWaypoint("p1", p1)));
 
 		WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<Waypoint>();
 		waypointPainter.setWaypoints(waypoints);
 		List<Painter<JXMapViewer>> painters = new ArrayList<Painter<JXMapViewer>>();
-        painters.add(waypointPainter);
+		painters.add(waypointPainter);
 
-        CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
-        viewer.setOverlayPainter(painter);
-        
-        }
-	
-	public void removeLocations() {
-		
+		CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
+		viewer.setOverlayPainter(painter);
+
 	}
 
+	public void removeLocations() {
+
+	}
+
+	/**
+	 * Adds double click and sets the action once double click occours
+	 */
 	public void addDoubleClick() {
 		viewer.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(final MouseEvent e) {
-				if(e.getButton() ==  1 && e.getButton() == 1) {
+				if (e.getButton() == 1 && e.getButton() == 1) {
 					eventCnt = e.getClickCount();
 					if (e.getClickCount() == 1) {
 						timer.schedule(new TimerTask() {
@@ -103,63 +102,71 @@ public class Viewer {
 								} else if (eventCnt > 1) {
 									Point p = e.getPoint();
 									Point2D pt = viewer.convertGeoPositionToPoint(Paris);
-//									System.out.println(p.distance(pt));
+
 									GeoPosition geo = viewer.convertPointToGeoPosition(p);
-									System.out.println("Distance in kilometers: " + distFrom(geo.getLatitude(), geo.getLongitude()
-											, Paris.getLatitude(), Paris.getLongitude()));
+									Guess guess = new Guess(geo.getLatitude(), geo.getLongitude());
+									System.out.println("Distance in kilometers: " + distFrom(geo.getLatitude(),
+											geo.getLongitude(), Paris.getLatitude(), Paris.getLongitude()));
 									addLocation(geo);
-									
-									
+
 								}
 								eventCnt = 0;
 							}
 						}, 400);
 					}
-				} if(e.getButton() == 3) {
+				}
+				if (e.getButton() == 3) {
 					System.out.println("right click");
 					removePaint();
 				}
-			}})	;	
-				
-				
+			}
+		});
+
 	}
-	
+
+	/**
+	 * Returns the JXMapviewer
+	 * 
+	 * @return {@link JXMapViewer}
+	 */
+
 	public JXMapViewer getViewer() {
 		return viewer;
 	}
-	
+
+	/**
+	 * Removes the marker that represents the position that the player clicks
+	 */
+
 	public void removePaint() {
 		List<Painter<JXMapViewer>> painters = new ArrayList<Painter<JXMapViewer>>();
-        CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
-        viewer.setOverlayPainter(painter);
-        painter.clearCache();
+		CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
+		viewer.setOverlayPainter(painter);
+		painter.clearCache();
 	}
-	
+
+	/**
+	 * Calculates the distance between two coordinates
+	 * 
+	 * @param lat1
+	 * @param lng1
+	 * @param lat2
+	 * @param lng2
+	 * @return distance in kilometers
+	 */
+
 	public static float distFrom(double lat1, double lng1, double lat2, double lng2) {
-	    double earthRadius = 6371000; //meters
-	    double dLat = Math.toRadians(lat2-lat1);
-	    double dLng = Math.toRadians(lng2-lng1);
-	    double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-	               Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-	               Math.sin(dLng/2) * Math.sin(dLng/2);
-	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-	    float dist = (float) (earthRadius * c);
-	    dist = dist / 1000;
-	    int distance = (int) dist;
+		double earthRadius = 6371000; // meters
+		double dLat = Math.toRadians(lat2 - lat1);
+		double dLng = Math.toRadians(lng2 - lng1);
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(lat1))
+				* Math.cos(Math.toRadians(lat2)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		float dist = (float) (earthRadius * c);
+		dist = dist / 1000;
+		int distance = (int) dist;
 
-	    return distance;
-	    }
+		return distance;
+	}
 
-	
-	
-
-    public static void main(String[] args){
-
-        Viewer viewer = new Viewer();
-        JFrame frame = new JFrame("GoGeo test1");
-        frame.getContentPane().add(viewer.getViewer());
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
 }
