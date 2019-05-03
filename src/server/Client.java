4064fc03000f520e847cd8799fda2ff49d0ba889
kevin <kevin@BOARDROOM>
2019-05-03 02:08:26 +0200

    .

--- src/server/Client.java
+++ src/server/Client.java
@@ -4,23 +4,39 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import client.Guess;
+
 public class Client {
 	private Socket socket;
 	private ObjectInputStream ois;
-	private ObjectOutputStream oos; 
+	private ObjectOutputStream oos;
+	private Guess guess;
+	private Receiver receiver;
 	
 	public Client(Socket socket) {
 		this.socket = socket;
+	}
+	
+	public void setGuess(Guess guess) {
+		this.guess = guess;
+	}
+	
+	private class Receiver extends Thread{
+		
+		public void run() {
+			while(true) {
+				try {
+					Guess guess = (Guess) ois.readObject();
+					setGuess(guess);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
 		
 	}
 	
-	private class receiver{
-		
-	}
-	
-	private class sender{
-		
-	}
+
 	
 	
 }
