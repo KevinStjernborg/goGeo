243e9a51f10fec6fc9e2e0dccbf8f6ad8d8b1166
Said Mohammed <said.mohammed@hotmail.com>
2019-05-28 11:38:18 +0200

    kommenterat controller

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,15 +1,22 @@
 package client;
 
+import java.util.Timer;
+
 import gui.GameW;
 import gui.GameWindowMP;
 import gui.GameWindowSP;
 import gui.Menu1new;
 import gui.Menu2new;
+import server.Game;
 import shared.Guess;
 import shared.Locations;
 import shared.Message;
 
-
+/**
+ * 
+ * @author 
+ *
+ */
 
 public class Controller {
 
@@ -21,19 +28,38 @@ public class Controller {
 	private GameW gameWindowMP;
 
 
+	/**
+	 * Starts the main menu when the {@link Controller} object is created. 
+	 */
 	public Controller() {
 		guiOne = new Menu1new(this);
 	}
 
+	/**
+	 * Sets the targeted city on the prompt Label on {@link GameW}. 
+	 * 
+	 * @param name of the city that the user should find. 
+	 */
 	public void setPromptInstruction(String name) {
 		locations.getLocation(name);
 		gameWindowMP.setInstruction(name);
 	}
 
+	/**
+	 * Sets the timer value on the timer label on {@link GameW}. 
+	 * 
+	 * @param the value that the timer will show. 
+	 */
 	public void setTimer(String nr) {
 		gameWindowMP.setTimerText(nr);
 	}
 
+	/**
+	 * Sets the current score for each player on {@link GameW}. 
+	 * 
+	 * @param score, the calculated score based on the players participation in the game. 
+	 * @param player, the player receiving the score. 
+	 */
 	
 	public void showPlayerScore(int score, int player) {
 		if (player == 1 || player == 2) {
@@ -41,7 +67,12 @@ public class Controller {
 		}
 	}
 
-	
+	/**
+	 * Sets the players name on the correct label on {@link GameW}. 
+	 * 
+	 * @param name, the identifier for the player
+	 * @param player, which player that gets the identifier. 
+	 */
 	
 	public void showPlayerName(String name, int player) {
 		if (player == 1 || player == 2) {
@@ -49,10 +80,21 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * Writes a new line on the console on the {@link GameW}.
+	 * 
+	 * @param message, the message that is shown. 
+	 */
 	public void showConsoleMessage(String message) {
 		gameWindowMP.setConsoleText(message);
 	}
 
+	/**
+	 * takes in a Guess object which is converted to a {@link Message} 
+	 * and sent through the server via {@link Connection}. 
+	 * 
+	 * @param guess 
+	 */
 	public void sendMessage(Guess guess) {
 		Message message = new Message();
 		message.setGuess(guess);
@@ -60,11 +102,19 @@ public class Controller {
 	}
 
 	
+	/**
+	 * Starts the second menu, {@link Menu2new}
+	 */
 
 	public void startGameModeMenu() {
 		guiTwo = new Menu2new(this);
 		
 	}
+	
+	/**
+	 * Initiates a multiplayer game by starting a {@link GameW} and setting up a connection 
+	 * through {@link Connection}
+	 */
 
 	public void startMultiplayerGame() {
 		gameWindowMP = new GameW(this, 1);
@@ -73,15 +123,24 @@ public class Controller {
 	}
 
 	public void startSinglePlayerMenu() {
-
 	}
 
+	/**
+	 * Whenever a {@link Timer} timed out this method requests the guess of the opponent player. 
+	 * 
+	 */
 	public void requestOtherPlayersGuess() {
 		Message message = new Message();
 		message.setRequestBooleanAsTrue();
 		connection.sendMessage(message);
 	}
 
+	/**
+	 * this method handles the {@link Message} object depending on the boolean values that
+	 * the object contains. 
+	 * 
+	 * @param message
+	 */
 	public void receiveMessage(Message message) {
 		if (message.containsGuess() && message.containsRequest() == false) {
 			Guess guess = message.getGuess();
